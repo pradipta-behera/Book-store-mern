244caad2c77265efabb76327f1c12cf2a1d3c413
pradipta-behera <pradiptabehera117@gmail.com>
2024-05-21 10:11:51 +0530

    added frontend

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from "react"
+import "./Home.css"
+
+const Home = () => {
+    return (
+        
+        <div className="Home-page text-white container-fluid ">
+            
+            <div className="row-container">
+                <div className="col-lg-6 d-flex mx-auto justify-content-center align-items-center flex-column">
+                    <h2>BOOK STORE FOR YOU</h2>
+                    <h3>Escape into a world of imagination—find your perfect book here.</h3> 
+                        <h3>Your next adventure awaits!</h3>
+                    <button className="Viewbook my-3">View Books</button>
+
+                </div>
+             
+
+            </div>
+
+        </div>
+    )};
+
+
+    export default Home;
